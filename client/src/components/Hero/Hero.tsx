aa0642dbb0f454e0be2a9fe00a7eff63d2f311dd
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-15 16:04:12 +0300

    Create and style hero component

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,7 +1,41 @@
+import styled from "styled-components";
+import { Container } from "react-bootstrap";
+
+const HeroComponent = styled.header`
+    padding: 5rem 0;
+    height: 80vh;
+    background-image: url("https://images.unsplash.com/photo-1523875194681-bedd468c58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80");
+    background-size: cover;
+    background-position: center;
+`;
+
+const HeaderContainer = styled.div`
+    background-color: rgb(5, 148, 112);
+    padding: 3rem;
+    color: white;
+    width: 26rem;
+`;
+const Heading = styled.h1`
+    font-size: 4rem;
+`;
+const SubHeading = styled.h3`
+    margin: 1rem 0;
+    font-weight: 400;
+`;
 
 const Hero = () => {
     return (
-        <div>Test</div>
+        <HeroComponent>
+            <Container>
+                <HeaderContainer>
+                    <Heading>Feed you mind with the best</Heading>
+                    <SubHeading>
+                        Grow, learn and become more successful by reading some
+                        of the top articles
+                    </SubHeading>
+                </HeaderContainer>
+            </Container>
+        </HeroComponent>
     );
 };
 
