79ca98f5c859f6c66081f5e125f71b05bafa6149
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-16 23:23:33 +0300

    Add mongoose and connect to DB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
 import authRoutes from "./routes/auth";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
-app.use(express.json());
-app.use("/auth", authRoutes);
 
-app.listen(8080, () => {
-    console.log("Listening on port 8080...");
-});
+mongoose
+    .connect(process.env.MONGO_URI as string)
+    .then(() => {
+        console.log("Connected to MongoDB");
+
+        const app = express();
+
+        app.use(express.json());
+        app.use("/auth", authRoutes);
+
+        app.listen(8080, () => {
+            console.log("Listening on port 8080...");
+        });
+    })
+    .catch(() => {
+        throw new Error();
+    });
