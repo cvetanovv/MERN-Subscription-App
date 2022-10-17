f64db6840aa9b2eeeeb86322e571d2c32a64ec17
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-17 19:19:57 +0300

    Handle CORS error

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,9 @@ import express from "express";
 import authRoutes from "./routes/auth";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
-
 
 mongoose
     .connect(process.env.MONGO_URI as string)
@@ -14,6 +14,7 @@ mongoose
         const app = express();
 
         app.use(express.json());
+        app.use(cors());
         app.use("/auth", authRoutes);
 
         app.listen(8080, () => {
