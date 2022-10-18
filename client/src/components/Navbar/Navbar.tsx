623dcb5bf494a8ecdad710882722c5b43d056068
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2022-10-18 17:06:26 +0300

    Add Logout

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,24 @@
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { UserContext } from "../../context";
+import styled from "styled-components";
+
+const LeftNavContainer = styled.div`
+    margin-left: auto;
+`
 
 const Navbar = () => {
     const [state, setState] = useContext(UserContext);
+
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        setState({data: null, loading: false, error: null})
+        localStorage.removeItem("token")
+        navigate("/")
+    }
     return (
         <Nav>
             <Nav.Item>
@@ -14,11 +27,13 @@ const Navbar = () => {
                 </Link>
             </Nav.Item>
             {state.data && (
-                <Nav.Item>
-                    <NavLink >
-                        Logout
-                    </NavLink>
-                </Nav.Item>
+                <LeftNavContainer>
+                    <Nav.Item>
+                        <NavLink onClick={handleLogout}>
+                            Logout
+                        </NavLink>
+                    </Nav.Item>
+                </LeftNavContainer>
             )}
         </Nav>
     );
